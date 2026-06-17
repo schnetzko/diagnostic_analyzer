5cbdcd70f55ecb903ab91dff5c3eeebdef52458d
Schnetzko <schnetzko@schnetzko.de>
2026-06-17 14:48:48 +0200

    extend log file generator by using random english words

--- tests/integration/generate_diagnostic_log_file.py
+++ tests/integration/generate_diagnostic_log_file.py
@@ -1,5 +1,6 @@
 from logging.handlers import RotatingFileHandler
 import logging, random
+from random_word import RandomWords
 
 diagnostic_log_file="ATE_diagnostic.log"
 date_format = "%Y-%m-%d %H:%M:%S"
@@ -23,7 +24,22 @@ file_handler = RotatingFileHandler(diagnostic_log_file, maxBytes=5000, backupCou
 file_handler.setFormatter(DiagnosticFormatter())
 logger.addHandler(file_handler)
 
-# 2. define log entries
+# 2. define english word generator for log entries
+def generate_random_english_words(number_of_words: int) -> str:   
+    random_words_generator = RandomWords()
+    random_words = []
+    
+    for i in range(number_of_words):
+        random_words.append(random_words_generator.get_random_word())
+
+    if not random_words:
+        return "Failed to fetch words."
+        
+    random_words_separated_by_spaces = " ".join(random_words)
+
+    return random_words_separated_by_spaces
+
+# 3. define log entries
 def create_PE_device_insertion_log_entry(device_id):
     logger.info("BEGIN INSERTION {device_id}".format(device_id=device_id), extra={'PE_format': True})
     create_any_other_log_entry()
@@ -46,7 +62,7 @@ def create_PE_device_removal_log_entry():
     create_any_other_log_entry()
     logger.info("END REMOVAL", extra={'PE_format': True})
 
-def create_PE_device_log_entry(device_id):
+def create_device_log_entry(device_id):
     create_any_other_log_entry()
     create_PE_device_insertion_log_entry(device_id)
     create_any_other_log_entry()
@@ -55,15 +71,16 @@ def create_PE_device_log_entry(device_id):
     create_PE_device_removal_log_entry()
 
 def create_any_other_log_entry():
-    number_of_entries = random.randint(1,5)
-    for i in range (number_of_entries):
-        logger.info("any other event log", extra={'PE_format': False})
+    number_of_log_entries = random.randint(0,5)
+    number_of_words_per_log_entry = random.randint(2,4)
+    for i in range (number_of_log_entries):
+        logger.info(generate_random_english_words(number_of_words_per_log_entry), extra={'PE_format': False})
 
 def create_diagnostic_log_file(number_of_devices):
     for i in range(number_of_devices):
-        create_PE_device_log_entry(f"\"Device #{i}\"")
+        create_device_log_entry(f"\"Device #{i}\"")
 
-# 3. create diagnostic log file
+# 4. create diagnostic log file
 number_of_devices = 10
 create_diagnostic_log_file(number_of_devices)
 print("Diagnostic log file", diagnostic_log_file, "has been generated successfully.")
